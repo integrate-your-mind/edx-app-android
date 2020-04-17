32e4367cac0eff451ca76cfefd779e9d8db0fd7e
Omer Habib <omer.habib26@gmail.com>
2020-04-17 16:16:53 +0500

    Refactor code

--- OpenEdXMobile/src/androidTest/java/org/edx/mobile/view/AccountActivityTest.kt
+++ OpenEdXMobile/src/androidTest/java/org/edx/mobile/view/AccountActivityTest.kt
@@ -8,11 +8,13 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import com.facebook.testing.screenshot.Screenshot
 import org.edx.mobile.R
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestName
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -23,8 +25,10 @@ class AccountActivityTest {
     @JvmField
     val rule: ActivityTestRule<AccountActivity> = ActivityTestRule(AccountActivity::class.java)
 
+    @Rule
+    var testName = TestName()
+
     private lateinit var activity: Activity
-    private lateinit var fragment: AccountFragment
 
     @Before
     fun before() {
@@ -58,14 +62,9 @@ class AccountActivityTest {
         Espresso.onView(ViewMatchers.withId(R.id.logout_btn)).perform(ViewActions.click())
     }
 
-
-//    @Rule
-//    var testName = TestName()
-//
-//    @Test
-//    @Throws(Throwable::class)
-//    fun testScreenshotRecordAccountActivity() {
-//        Screenshot.snap(activity.findViewById(R.id.my_groups_list_container)).setName(javaClass.name + "_" + testName.methodName).record()
-////        activity.runOnUiThread { Screenshot.snap(activity.findViewById(R.id.my_groups_list_container)).setName(javaClass.name + "_" + testName.methodName).record() }
-//    }
+    @Test
+    @Throws(Throwable::class)
+    fun testScreenshotRecordAccountActivity() {
+        activity.runOnUiThread { Screenshot.snap(activity.findViewById(R.id.my_groups_list_container)).setName(javaClass.name + "_" + testName.methodName).record() }
+    }
 }
